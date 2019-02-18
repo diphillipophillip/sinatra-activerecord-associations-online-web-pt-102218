a469209c6ae6ab722fb01e185e8db46a0b3b08e7
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-02-18 18:05:25 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915202821) do
-
-  create_table "cats", force: :cascade do |t|
-    t.string  "name"
-    t.integer "age"
-    t.string  "breed"
-    t.integer "owner_id"
-  end
-
-  create_table "owners", force: :cascade do |t|
-    t.string "name"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
